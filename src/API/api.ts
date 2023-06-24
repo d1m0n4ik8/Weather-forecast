1fe8dd246c75774262cd4faaaa2469e123bf391e
d1m0n4ik <svyryd2012@gmail.com>
2023-06-24 18:36:37 +0300

    CurrentWeather

--- src/API/api.ts
+++ src/API/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
-import { IGeoDataItem } from '../Interfaces/IGeo'
+import { IGeoData } from '../Interfaces/IGeo'
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
 
 export const geoApiOptions = {
 	params: {
@@ -13,17 +14,20 @@ export const geoApiOptions = {
 	},
 }
 
-export const loadOptions = (inputValue: string) => {
-	return axios.get(`${GEO_API_URL}/cities?namePrefix=${inputValue}`, geoApiOptions).then(response => {
-		console.log(response.data)
+export const weatherApiOptions = {
+	params: {
+		appid: '193a709fb7933966f68b98b03a35dc85',
+	},
+}
 
+export const loadOptions = (inputValue: string) =>
+	axios.get<IGeoData>(`${GEO_API_URL}/cities?namePrefix=${inputValue}`, geoApiOptions).then(response => {
 		return {
-			options: response.data.data.map((city: IGeoDataItem) => {
-				return {
-					value: `${city.latitude} ${city.longitude}`,
-					label: `${city.name}, ${city.countryCode}`,
-				}
-			}),
+			options: response.data.data.map(city => ({
+				value: `${city.latitude} ${city.longitude}`,
+				label: `${city.name}, ${city.countryCode}`,
+			})),
 		}
 	})
-}
+
+//export const getCurrentWeather = () => {}
